1410595078e7e3fd5ece84bc3d417dc08bbe7dbd
arfaro <cikasarung@gmail.com>
2020-10-13 16:25:23 +0700

    perbaiki login.php

---
+++ login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Aplikasi Penggajian</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    
+  </head>
+  <style>
+      body{
+        background: url(assets/img/backgroundkal.jpg) center fixed no-repeat;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+      }
+      
+    </style>
+  <body>
+    <div class="container">
+      <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title"><span class="glyphicon glyphicon-lock"></span>LOGIN APLIKASI GAJI</h3>
+      </div>
+      <div class="panel-body">
+        <form action="" method="post" role="form">
+          <div class="form-group">
+            <input type="text" name="username" class="form-control" placeholder="Username">
+          </div>
+          <div class="form-group">
+            <input type="text" name="password" class="form-control" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <input type="submit" name="username" class="btn btn-primary btn-lg btn-block" value="Login">
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
+
+    <script src="assets/js/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </body>
+</html>
